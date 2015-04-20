9619669ad3d21e66b0e71d39b3b597ed792a8833
CodyIsAwesome <cchilder@ucsc.edu>
2015-04-20 13:16:14 -0700

    Generalized program
    You can now specify the size of the dice, how many dice (still assumes
    you are taking away only 1), and how many players you want to roll for,
    and it'll roll separate stats for each player.

--- rollstats.py
+++ rollstats.py
@@ -2,17 +2,26 @@
 #rolls 3d8, takes the best 2 for each stat
 #runs for as many stats as the user inputs
 import random
+#constants
 diceSize = 8
+diceCount = 3
+#get input
 numToRoll = int(input("How many stats do you want to roll? "))
-print("Your rolls are...")
-for i in range(0, numToRoll):
-    sum = 0
-    min = 99999999
-    for j in range(0, 3):
-        val = random.randint(1, diceSize)
-        sum += val
-        if val < min:
-            min = val
-    sum -= min
-    print(sum)
+playersToRoll = int(input("How many players do you want to roll for? "))
+#run calculations
+for player in range(0, playersToRoll):
+    print("Your rolls for player", player + 1, "are...")
+    for i in range(0, numToRoll):
+        sum = 0
+        min = 99999999
+        #this loop just adds all the rolls together, but records which number was smallest
+        #after it runs, it subtracts that from the sum, which effectively is picking the best of the rolls
+        for j in range(0, diceCount):
+            val = random.randint(1, diceSize)
+            sum += val
+            if val < min:
+                min = val
+        sum -= min
+        print(sum)
+    print("", end="\n")
 input("Press enter to exit\n")
