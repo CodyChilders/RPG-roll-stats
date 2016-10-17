0a88ce821a83a886ac904c03bbd0135cf072b3b8
CodyIsAwesome <cchilder@ucsc.edu>
2016-10-16 22:17:26 -0700

    Smarter algorithms
    - smarter printing, only best and worst 25, instead of dumping the whole
    batch
    - built in sort instead of bubble sort
    - optimized memory so large batches can be used
    - removed cap on largest input so it can run huge batches.

--- rollstats.py
+++ rollstats.py
@@ -27,29 +27,45 @@ def getInt(prompt):
             val = int(input(prompt))
             if val <= 0:
                 print("Please enter a positive integer")
-            elif val > 100:
-                print("Please use reasonable numbers that are <100")
+            #elif val > 1000000:
+            #    print("Please use reasonable numbers that are <1000000")
             else:
                 return val
         except ValueError:
             print("Please enter a positive integer")
 
 def printStatBlockList(statBlockList):
-    player = 1
-    for s in statBlockList:
-        print("Your rolls for player", player, "are...")
-        s.PrintStats()
-        print("The sum of the stats is", s.GetSum(), end="\n\n")
-        player += 1
+    if len(statBlockList) < 50:
+        #loop through all, and print the entire set
+        player = 0
+        for s in statBlockList:
+            print("Your rolls for player", player + 1, "are...")
+            s.PrintStats()
+            print("The sum of the stats is", s.GetSum(), end="\n\n")
+            player += 1
+    else:
+        #only print the lowest and highest 25
+        print("Lowest 25 stats", end="\n\n")
+        player = 0
+        for s in statBlockList: #print the first 25
+            print("Your rolls for player", player + 1, "are...")
+            s.PrintStats()
+            print("The sum of the stats is", s.GetSum(), end="\n\n")
+            player += 1
+            if player > 25:
+                break
+        for i in range(0, 50):
+            print('=', end='')
+        print("\nHighest 25 stats", end="\n\n")
+        player = len(statBlockList) - 25
+        while player < len(statBlockList):
+            print("Your rolls for player", player + 1, "are...")
+            statBlockList[player].PrintStats()
+            print("The sum of the stats is", statBlockList[player].GetSum(), end="\n\n")
+            player += 1
 
-#implements bubble sort, because I'm lazy
 def sortStats(statBlockList):
-    for passnum in range(len(statBlockList) - 1, 0, -1):
-        for i in range(passnum):
-            if statBlockList[i].GetSum() > statBlockList[i+1].GetSum():
-                temp = statBlockList[i]
-                statBlockList[i] = statBlockList[i+1]
-                statBlockList[i+1] = temp
+    return sorted(statBlockList, key=lambda StatBlock: StatBlock.GetSum())
 
 def main():
     #setup
@@ -73,8 +89,9 @@ def main():
             sum -= min
             newStatBlock.AddStat(sum)
         allStatBlocks.append(newStatBlock)
-    sortStats(allStatBlocks)
-    printStatBlockList(allStatBlocks)
+    sortedStats = sortStats(allStatBlocks)
+    allStatBlocks = [] #i needs memory plz
+    printStatBlockList(sortedStats)
     input("Press enter to exit\n")
 
 main()
